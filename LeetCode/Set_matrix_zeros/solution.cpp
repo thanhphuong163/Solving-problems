d027750e085a2fa8019bcd2bc779c9660bd1878a
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-03-31 18:51:29 +0700

    Initialize

---
+++ LeetCode/Set_matrix_zeros/solution.cpp
@@ -0,0 +1,24 @@
+// Author: Nguyen Thanh Phuong
+// Email: thanhphuong.its@gmail.com
+// Problem: https://leetcode.com/problems/set-matrix-zeroes/
+// Submit:
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void set_zeros(vector< vector<int> > & matrix) {
+
+}
+
+int main(int argc, char const *argv[]) {
+    int n_testcase;
+    scanf("%d", &n_testcase);
+    while (n_testcase > 0) {
+        /* your code */
+        n_testcase--;
+    }
+    return 0;
+}
