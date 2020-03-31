ba72878b239ebe4950d1e72d70dd52e7292661f0
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-03-31 21:45:51 +0700

    solve problem with memory complexity O(1)

--- LeetCode/Set_matrix_zeros/solution.cpp
+++ LeetCode/Set_matrix_zeros/solution.cpp
@@ -53,33 +53,82 @@ void set_zeros_2(vector< vector<int> >& matrix) {
     // Where H and W are sizes of the matrix
     const int H = matrix.size();
     const int W = matrix[0].size();
-    vector<bool> row_zero(H);
-    
+    vector<bool> column_zero(W);
+
     for (int row = 0; row < H; ++row)
     {
         for (int col = 0; col < W; ++col)
         {
             if (matrix[row][col] == 0)
             {
-                row_zero[row] = true;
+                column_zero[col] = true;
             }
         }
     }
     for (int row = 0; row < H; ++row)
     {
-        bool contains_zeros = false;
+        bool row_contains_zeros = false;
         for (int col = 0; col < W; ++col) {
-            if (matrix[row][col] == 0) contains_zeros = true;
-            break;
+            if (matrix[row][col] == 0) {
+                row_contains_zeros = true;
+                break;
+            }
         }
         for (int col = 0; col < W; ++col)
         {
-            if (row_zero[row] || contains_zeros)
+            if (row_contains_zeros || column_zero[col])
             {
                 matrix[row][col] = 0;
             }
         }
     }
+    print_matrix(matrix);
+}
+
+void set_zeros_3(vector<vector<int>> &matrix)
+{
+    // This is a solution with space complexity O(1),
+    // Where H and W are sizes of the matrix
+    const int H = matrix.size();
+    const int W = matrix[0].size();
+
+    bool first_row_zero = false;
+    for (int col = 0; col < W; ++col)
+    {
+        if (matrix[0][col] == 0) {
+            first_row_zero = true;
+            break;
+        }
+    }
+    // Use the first row as a vector marking columns containing zeros
+    for (int row = 0; row < H; ++row) {
+        for (int col = 0; col < W; ++col) {
+            if (matrix[row][col] == 0) {
+                matrix[0][col] = 0;
+            }
+        }
+    }
+
+    for (int row = 1; row < H; ++row)
+    {
+        bool row_contains_zeros = false;
+        for (int col = 0; col < W; ++col)
+        {
+            if (matrix[row][col] == 0)
+            {
+                row_contains_zeros = true;
+                break;
+            }
+        }
+        for (int col = 0; col < W; ++col)
+        {
+            if (row_contains_zeros || matrix[0][col] == 0)
+            {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
     print_matrix(matrix);
 }
 
@@ -97,7 +146,7 @@ int main(int argc, char const *argv[]) {
         }
         print_matrix(matrix);
         cout << endl;
-        set_zeros_2(matrix);
+        set_zeros_3(matrix);
         n_testcase--;
     }
     return 0;
