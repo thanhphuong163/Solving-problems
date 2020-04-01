79ecce1296a9dc1ca0a0de6f2dd2ac6b9729e66b
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-01 14:48:09 +0700

    solved single number problem

--- LeetCode/Single_number/solution.cpp
+++ LeetCode/Single_number/solution.cpp
@@ -33,7 +33,7 @@ int main(int argc, char const *argv[]) {
         while (ss >> i) {
             nums.push_back(i);
         }
-        singleNumber(nums);
+        cout << singleNumber(nums) << endl;
         n_testcase--;
     }
     return 0;
