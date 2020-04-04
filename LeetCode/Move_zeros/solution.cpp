ad774269efed62fa9899c06f9c1a8ef46ae6684c
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-04 15:02:17 +0700

    solved move zeros problem

--- LeetCode/Move_zeros/solution.cpp
+++ LeetCode/Move_zeros/solution.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-vector<int> &read_array(string line)
+vector<int> read_array(string line)
 {
     stringstream ss(line);
     int num;
@@ -23,13 +23,41 @@ vector<int> &read_array(string line)
     return nums;
 }
 
+void print_array(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int& a, int& b) {
+    int t = b;
+    b = a;
+    a = t;
+}
+
+void move_zeros(vector<int>& nums) {
+    int size = nums.size();
+    int nz = 0;
+    int i = 0;
+    while (nz < size) {
+        if (nums[nz] != 0)
+            swap(nums[nz], nums[i++]);
+        nz++;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int n_testcase;
-    scanf("%dn", &n_testcase);
+    scanf("%d\n", &n_testcase);
     while (n_testcase--)
     {
-        /* your code */
+        string line;
+        getline(cin, line);
+        vector<int> nums = read_array(line);
+        move_zeros(nums);
+        print_array(nums);
     }
     return 0;
 }
