bf4665fb7e917e0ef4fd7db164379b05ef1a67af
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-04 18:20:25 +0700

    Initialize Sereja_and_Dima

---
+++ CodeForces/Sereja_and_Dima/solution.cpp
@@ -0,0 +1,32 @@
+// Author: Nguyen Thanh Phuong
+// Email: thanhphuong.its@gmail.com
+// Problem:
+// Submit:
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int>& read_array(string line) {
+stringstream ss(line);
+int num;
+vector<int> nums;
+while (ss >> num) {
+nums.push_back(num);
+}
+return nums;
+}
+
+int main(int argc, char const *argv[]) {
+int n_testcase;
+scanf("%d
+", &n_testcase);
+while (n_testcase--) {
+/* your code */
+}
+return 0;
+}
