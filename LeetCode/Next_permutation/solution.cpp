f961ea8aad7654ccf6a0d15621e50f5bc3f7963e
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-06 23:08:50 +0700

    run test

--- LeetCode/Next_permutation/solution.cpp
+++ LeetCode/Next_permutation/solution.cpp
@@ -66,7 +66,7 @@ int main(int argc, char const *argv[])
             if (i != nums.size() - 1)
                 cout << i << " ";
             else
-                cout << i << endl;
+                cout << i;
         }
         puts("");
     }
