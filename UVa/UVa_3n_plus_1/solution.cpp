d87f0746d84af2cbbc804452e4c56f3c6948889b
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-06 13:51:46 +0700

    run test cases

--- UVa/UVa_3n_plus_1/solution.cpp
+++ UVa/UVa_3n_plus_1/solution.cpp
@@ -10,6 +10,8 @@
 #include <string>
 #include <vector>
 
+#define max(a, b) (a > b ? a : b)
+
 using namespace std;
 
 void read_array(string line, vector<int> &nums)
@@ -22,6 +24,30 @@ void read_array(string line, vector<int> &nums)
     }
 }
 
+int get_cycle_length(int num)
+{
+    int length = 1;
+    while (num != 1)
+    {
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = 3 * num + 1;
+        length++;
+    }
+    return length;
+}
+
+int max_cycle_length(int i, int j)
+{
+    int max_cylen = 0;
+    for (int num = i; num <= j; num++)
+    {
+        max_cylen = max(max_cylen, get_cycle_length(num));
+    }
+    return max_cylen;
+}
+
 int main(int argc, char const *argv[])
 {
     int n;
@@ -31,7 +57,7 @@ int main(int argc, char const *argv[])
     {
         int i, j;
         cin >> i >> j;
-        printf("%d %d %d\n", i, j, 100);
+        printf("%d %d %d\n", i, j, max_cycle_length(i, j));
     }
     return 0;
 }
