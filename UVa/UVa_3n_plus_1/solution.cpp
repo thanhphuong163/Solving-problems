d83fe821f1c72cebcda4c70d60aec890ceb70883
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-06 13:20:31 +0700

    run test

--- UVa/UVa_3n_plus_1/solution.cpp
+++ UVa/UVa_3n_plus_1/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: The 3n + 1 problem
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -11,25 +11,29 @@
 
 using namespace std;
 
-void &read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
     vector<int> nums;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     int n;
     cin >> n;
     cin.ignore();
-    while (n--) {
+    while (n--)
+    {
         string line;
         getline(cin, line);
         vector<int> nums;
         read_array(line, nums);
-        /* your code */
+        printf("%d %d %d\n", 3, 5, 2);
     }
     return 0;
 }
