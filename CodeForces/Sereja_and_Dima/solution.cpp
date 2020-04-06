5c0bc3115c0365680c8885701ae2ffb09acf0e3a
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-06 12:44:32 +0700

    solved sereja and dima problem

--- CodeForces/Sereja_and_Dima/solution.cpp
+++ CodeForces/Sereja_and_Dima/solution.cpp
@@ -1,6 +1,7 @@
+//https://codeforces.com/problemset/problem/381/A
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem:
+// Problem: Sereja and Dima
 // Submit:
 
 #include <stdio.h>
@@ -11,22 +12,62 @@
 
 using namespace std;
 
-vector<int>& read_array(string line) {
-stringstream ss(line);
-int num;
-vector<int> nums;
-while (ss >> num) {
-nums.push_back(num);
-}
-return nums;
+vector<int> play_game(vector<int> &cards)
+{
+    int sereja = 0;
+    int dima = 0;
+    int l = 0;
+    int r = cards.size() - 1;
+    bool is_sereja_turn = true;
+    while (l <= r)
+    {
+        if (is_sereja_turn)
+        {
+            if (cards[l] > cards[r])
+            {
+                sereja += cards[l];
+                l++;
+            }
+            else
+            {
+                sereja += cards[r];
+                r--;
+            }
+            is_sereja_turn = false;
+        }
+        else
+        {
+            if (cards[l] > cards[r])
+            {
+                dima += cards[l];
+                l++;
+            }
+            else
+            {
+                dima += cards[r];
+                r--;
+            }
+            is_sereja_turn = true;
+        }
+    }
+    vector<int> ans;
+    ans.push_back(sereja);
+    ans.push_back(dima);
+    return ans;
 }
 
-int main(int argc, char const *argv[]) {
-int n_testcase;
-scanf("%d
-", &n_testcase);
-while (n_testcase--) {
-/* your code */
-}
-return 0;
+int main(int argc, char const *argv[])
+{
+    int n_testcase;
+    scanf("%d\n", &n_testcase);
+    vector<int> cards;
+    while (n_testcase--)
+    {
+        int card;
+        cin >> card;
+        cards.push_back(card);
+    }
+    vector<int> ans = play_game(cards);
+    cout << ans[0] << " " << ans[1] << endl;
+    return 0;
 }
