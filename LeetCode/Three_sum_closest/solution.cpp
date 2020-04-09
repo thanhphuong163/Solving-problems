e46a260607ac06740459a5a6e97f3b9b563ae3a9
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-09 21:37:11 +0700

    Three_sum_closest: try two pointers technique

--- LeetCode/Three_sum_closest/solution.cpp
+++ LeetCode/Three_sum_closest/solution.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -23,6 +24,35 @@ void read_array(string line, vector<int> &nums)
 
 int three_sum_closest(vector<int> &nums, int target)
 {
+    int size = nums.size();
+    int sum, min_diff = 10 ^ 6;
+    // sort the array
+    sort(nums.begin(), nums.end());
+    // Two pointers technique gives the O(n^2) solution
+    for (int i = 0; i < size; i++)
+    {
+        while (nums[i] == nums[i + 1])
+            i++; // skip repeating number
+        int t_target = target - nums[i];
+        int l = i + 1;
+        int r = size - 1;
+        do
+        {
+            int t_sum = nums[l] + nums[r];
+            if (abs(t_sum - t_target) < min_diff)
+            {
+                sum = nums[i] + t_sum;
+                min_diff = abs(t_sum - t_target);
+            }
+            while (nums[l] == nums[l + 1])
+                l++; // skip repeating numbers
+            while (nums[r] == nums[r - 1])
+                r--; // skip repeating numbers
+            l++;
+            r--;
+        } while (l < r);
+    }
+    return sum;
 }
 
 int main(int argc, char const *argv[])
@@ -32,6 +62,7 @@ int main(int argc, char const *argv[])
     {
         cin.ignore();
         string line;
+        getline(cin, line);
         vector<int> nums;
         read_array(line, nums);
         cout << three_sum_closest(nums, target) << endl;
