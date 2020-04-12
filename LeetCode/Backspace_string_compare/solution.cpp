da396a56ddb2e951fb0cd9565a0eb0bec8f8bbb5
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-12 16:09:30 +0700

    Backspace_string_compare: test solution

--- LeetCode/Backspace_string_compare/solution.cpp
+++ LeetCode/Backspace_string_compare/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/backspace-string-compare/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +12,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -30,14 +33,60 @@ void print_array(vector<int> &nums) {
 }
 
 /* Write your solution here */
-
-int main(int argc, char const *argv[]) {
-    string line;
-    while (getline(cin, line))
+bool backspaceCompare(string S, string T)
+{
+    int k = 0;
+    int l = 0;
+    for (int i = 0; i < S.length(); i++)
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        if (S[i] == '#')
+        {
+            k--;
+            k = max(k, 0);
+        }
+        else
+        {
+            S[k] = S[i];
+            k++;
+        }
+    }
+    for (int i = 0; i < T.length(); i++)
+    {
+        if (T[i] == '#')
+        {
+            l--;
+            l = max(l, 0);
+        }
+        else
+        {
+            T[l] = T[i];
+            l++;
+        }
+    }
+    if (k != l)
+        return false;
+    else
+    {
+        for (int i = 0; i < k; i++)
+        {
+            if (S[i] != T[i])
+                return false;
+        }
+        return true;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    string S;
+    while (getline(cin, S))
+    {
+        string T;
+        getline(cin, T);
+        if (backspaceCompare(S, T))
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
     }
     return 0;
 }
