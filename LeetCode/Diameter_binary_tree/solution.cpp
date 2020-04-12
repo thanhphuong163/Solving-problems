296b16db1856167f0f8416b86fc6558318fd63af
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-12 16:44:45 +0700

    add solution

--- LeetCode/Diameter_binary_tree/solution.cpp
+++ LeetCode/Diameter_binary_tree/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem:
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +12,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -31,7 +34,44 @@ void print_array(vector<int> &nums) {
 
 /* Write your solution here */
 
-int main(int argc, char const *argv[]) {
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+    int max_diameter = 0;
+    int get_height(TreeNode *node)
+    {
+        if (node == NULL)
+        {
+            max_diameter = max(0, max_diameter);
+            return 0;
+        }
+        else
+        {
+            int left_height = get_height(node->left);
+            int right_height = get_height(node->right);
+            max_diameter = max(max_diameter, left_height + right_height);
+            return max(1 + left_height, 1 + right_height);
+        }
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int tree_height = get_height(root);
+        return max_diameter;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
     string line;
     while (getline(cin, line))
     {
