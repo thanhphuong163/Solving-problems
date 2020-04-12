00e27e56b013e2f248f8420082388441867dde6b
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-12 16:20:10 +0700

    Min_stack: test solution

--- LeetCode/Backspace_string_compare/Min_stack/solution.cpp
+++ LeetCode/Backspace_string_compare/Min_stack/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem:
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +12,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -30,14 +33,75 @@ void print_array(vector<int> &nums) {
 }
 
 /* Write your solution here */
-
-int main(int argc, char const *argv[]) {
-    string line;
-    while (getline(cin, line))
+class MinStack
+{
+public:
+    /** initialize your data structure here. */
+    struct Node
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        int data;
+        int min;
+        Node *next;
+    };
+
+    Node *head;
+
+    MinStack()
+    {
+        this->head = NULL;
     }
+
+    void push(int x)
+    {
+        Node *node = new Node();
+        node->data = x;
+        if (!this->head)
+        {
+            node->min = x;
+        }
+        else
+        {
+            if (x < this->head->min)
+            {
+                node->min = x;
+            }
+            else
+            {
+                node->min = this->head->min;
+            }
+        }
+
+        node->next = this->head;
+        this->head = node;
+    }
+
+    void pop()
+    {
+        Node *node = this->head;
+        this->head = node->next;
+        delete node;
+    }
+
+    int top()
+    {
+        return this->head->data;
+    }
+
+    int getMin()
+    {
+        return this->head->min;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    MinStack minStack = MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    cout << minStack.getMin() << endl;
+    minStack.pop();
+    cout << minStack.top() << endl;
+    cout << minStack.getMin() << endl;
     return 0;
 }
