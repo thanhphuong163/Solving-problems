35b6b93e639b2bddc1bfc4ab2dbe207cc350481e
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-14 15:38:21 +0700

    Search_in_rotated_sorted_array: test solution

--- LeetCode/Search_in_rotated_sorted_array/solution.cpp
+++ LeetCode/Search_in_rotated_sorted_array/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/search-in-rotated-sorted-array/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -29,9 +29,65 @@ void print_array(vector<int> &nums) {
     puts("");
 }
 
-/* Write your solution here */
+int bin_search(vector<int> &nums, int left, int right, int target) {
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (target == nums[mid])
+        {
+            return mid;
+        }
+        else if (target > nums[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+/*
+Using binary search can get time complexity O(logN).
+Find the pivot and search in one of the two parts
+*/
+int get_index(vector<int> &nums, int target) {
+    if (nums.size() == 0) return -1;
+    if (nums.size() == 1) {
+        return nums[0] == target ? 0 : -1;
+    }
+    int p = 0;
+    int size = nums.size();
+    for (int i = 0; i < size - 1; i++) {
+        if (nums[i] > nums[i+1]) {
+            p = i + 1;
+            break;
+        }
+    }
+    if (p == 0)
+        return bin_search(nums, 0, size-1, target);
+    
+    if (nums[0] <= target && target <= nums[p-1]) {
+        return bin_search(nums, 0, p-1, target);
+    }
+    else if (nums[p] <= target && target <= nums[size-1]) {
+        return bin_search(nums, p, size -1, target);
+    }
+    else {
+        return -1;
+    }
+}
+
+// #define DEBUG_MODE
 
 int main(int argc, char const *argv[]) {
+#ifdef DEBUG_MODE
+    int target = 2;
+    vector<int> nums = {3,4,1};
+    cout << get_index(nums, target) << endl;
+#else   // test mode
     string line;
     while (getline(cin, line))
     {
@@ -39,5 +95,6 @@ int main(int argc, char const *argv[]) {
         read_array(line, nums);
         /* your code here */
     }
+#endif
     return 0;
 }
