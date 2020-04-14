c84d436de6c7619610719a9be381ba49af4d194e
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-14 14:30:46 +0700

    Initialize Perform_string_shift

---
+++ LeetCode/Perform_string_shift/solution.cpp
@@ -0,0 +1,45 @@
+// Author: Nguyen Thanh Phuong
+// Email: thanhphuong.its@gmail.com
+// Problem: 
+// Submit: 
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Read input
+void read_array(string line, vector<int> &nums) {
+    stringstream ss(line);
+    int num;
+    while (ss >> num) {
+    	nums.push_back(num);
+    }
+}
+
+// Print out input
+void print_array(vector<int> &nums) {
+    for (int i : nums)
+    {
+        cout << i << " ";
+    }
+    puts("");
+}
+
+/*
+Write your solution here
+*/
+
+int main(int argc, char const *argv[]) {
+    string line;
+    while (getline(cin, line))
+    {
+        vector<int> nums;
+        read_array(line, nums);
+        /* your code here */
+    }
+    return 0;
+}
