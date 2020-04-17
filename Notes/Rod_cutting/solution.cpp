96026b961963604a0c0423a05965a8d0a75691a0
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-17 22:30:29 +0700

    Rod_cutting: test dynamic programming solution

--- Notes/Rod_cutting/solution.cpp
+++ Notes/Rod_cutting/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: Rod Cutting
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +12,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -32,18 +35,39 @@ void print_array(vector<int> &nums) {
 /*
 Write your solution here
 */
+int rodCutting(const vector<int> prices, int length, int cost)
+{
+    vector<int> r(length + 1, 0);
+    for (int j = 0; j <= length; j++)
+    {
+        int q = INT_MIN;
+        for (int i = 0; i <= j; i++)
+        {
+            if (q < prices[i] + r[j - i] - cost)
+            {
+                q = prices[i] + r[j - i] - cost;
+            }
+        }
+        r[j] = q;
+    }
+    return r[length];
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
     string line;
-    while (getline(cin, line))
+    vector<int> prices;
+    getline(cin, line);
+    read_array(line, prices);
+    int length;
+    while (cin >> length)
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        cout << rodCutting(prices, length, 0) << endl;
     }
 #endif
     return 0;
