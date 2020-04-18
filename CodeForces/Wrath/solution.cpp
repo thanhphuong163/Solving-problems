496b6f20ab501aa2d462f128ce5d218b30f8b17b
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-18 23:37:51 +0700

    Wrath: test solution

--- CodeForces/Wrath/solution.cpp
+++ CodeForces/Wrath/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://codeforces.com/problemset/problem/892/B
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -32,18 +32,36 @@ void print_array(vector<int> &nums) {
 /*
 Write your solution here
 */
+int kill(vector<int> &nums) {
+    int n = nums.size();
+    int i = 2;
+    int j = 1;
+    while (i <= nums.size()) {
+        if (j < i && j >= i - nums[i - 1])
+        {
+            n--;
+            j++;
+        }
+        else
+            i++;
+    }
+    return n;
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
-    string line;
-    while (getline(cin, line))
+    int n;
+    while (cin >> n)
     {
+        cin.ignore();
+        string line;
+        getline(cin, line);
         vector<int> nums;
         read_array(line, nums);
-        /* your code here */
+        cout << kill(nums) << endl;
     }
 #endif
     return 0;
