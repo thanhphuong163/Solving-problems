258e0d2009febcd3c756726b080fee8baf34dc4c
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-18 16:16:29 +0700

    Min_sum_path: test iterative DP solution

--- LeetCode/Min_sum_path/solution.cpp
+++ LeetCode/Min_sum_path/solution.cpp
@@ -1,13 +1,17 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/minimum-path-sum/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
+
+#define vi vector<int>
+#define vvi vector<vi>
 
 using namespace std;
 
@@ -32,18 +36,49 @@ void print_array(vector<int> &nums) {
 /*
 Write your solution here
 */
+int minPathSum(vvi &grid) {
+    int rows = grid.size();
+    if (rows == 0) return 0;
+    int cols = grid[0].size();
+    if (cols == 0) return 0;
+    vvi dp;
+    for (int i = 0; i < rows; i++) {
+        vi row(cols, INT_MAX);
+        dp.push_back(row);
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (i==0 && j==0)
+                dp[i][j] = grid[0][0];
+            else {
+                int above = i > 0 ? dp[i-1][j] : INT_MAX;
+                int left = j > 0 ? dp[i][j-1] : INT_MAX;
+                dp[i][j] = grid[i][j] + min(above, left);
+            }
+        }
+    }
+
+    return dp[rows-1][cols-1];
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
-    string line;
-    while (getline(cin, line))
+    int rows, cols;
+    while (cin >> rows >> cols)
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        string line;
+        vvi grid;
+        while (getline(cin, line)) {
+            vi row;
+            read_array(line, row);
+            grid.push_back(row);
+        }
+        
+        cout << minPathSum(grid) << endl;
     }
 #endif
     return 0;
