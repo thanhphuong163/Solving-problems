8b1ae049df735101b934e155f23be4eeee2169d5
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-19 16:26:48 +0700

    Eugeny_and_playlist: solved

--- CodeForces/Eugeny_and_playlist/solution.cpp
+++ CodeForces/Eugeny_and_playlist/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://codeforces.com/problemset/problem/302/B
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -14,16 +14,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -32,29 +35,36 @@ void print_array(vector<int> &nums) {
 }
 
 /*
+    Using prefix and two-pointer technique
     pref:   0 2 4 5 9
     moment: 1 2 3 4 5 6 7 8 9
 */
-void getSongAtMoment(vi &c, vi &t, vi &moments) {
-    vi pref(c.size()+1, 0);
-    for (int i = 1; i <= c.size(); i++) {
-        pref[i] = pref[i-1] + c[i-1] * t[i-1];
+void getSongAtMoment(vi &c, vi &t, vi &moments)
+{
+    vi pref(c.size() + 1, 0);
+    for (int i = 1; i <= c.size(); i++)
+    {
+        pref[i] = pref[i - 1] + c[i - 1] * t[i - 1];
     }
     int j = 0;
     int i = 0;
-    while (i < moments.size()) {
-        if (moments[i] <= pref[j] || j >= pref.size()) {
+    while (i < moments.size())
+    {
+        if (moments[i] <= pref[j] || j >= pref.size())
+        {
             cout << j << endl;
             i++;
         }
-        else {
+        else
+        {
             j++;
         }
     }
 }
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
@@ -62,8 +72,9 @@ int main(int argc, char const *argv[]) {
     while (cin >> n >> m)
     {
         cin.ignore();
-        vector<int> c,t;
-        while (n--) {
+        vector<int> c, t;
+        while (n--)
+        {
             int c_, t_;
             cin >> c_ >> t_;
             cin.ignore();
@@ -74,7 +85,7 @@ int main(int argc, char const *argv[]) {
         getline(cin, line);
         vector<int> moments;
         read_array(line, moments);
-        getSongAtMoment(c,t,moments);
+        getSongAtMoment(c, t, moments);
     }
 #endif
     return 0;
