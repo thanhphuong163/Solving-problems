52750ecbc2a76ad66d448c40725e5319ead1eec2
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-19 17:15:33 +0700

    Summing_N_series: try unsigned long long int

--- HackerRank/Summing_N_series/solution.cpp
+++ HackerRank/Summing_N_series/solution.cpp
@@ -40,7 +40,7 @@ int summingSeries(long n)
     long c = (long)1e9 + 7;
     unsigned long long int a = n%c;
     unsigned long long int res = (a*a)%c;
-    return a;
+    return res;
 }
 
 // #define DEBUG_MODE
