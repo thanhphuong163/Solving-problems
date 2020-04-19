415d5b864204ea2d832bb896f5fd484770e85112
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-19 16:21:29 +0700

    Eugeny_and_playlist: test solution using prefix and two pointers technique

--- CodeForces/Eugeny_and_playlist/solution.cpp
+++ CodeForces/Eugeny_and_playlist/solution.cpp
@@ -9,6 +9,8 @@
 #include <string>
 #include <vector>
 
+#define vi vector<int>
+
 using namespace std;
 
 // Read input
@@ -30,20 +32,49 @@ void print_array(vector<int> &nums) {
 }
 
 /*
-Write your solution here
+    pref:   0 2 4 5 9
+    moment: 1 2 3 4 5 6 7 8 9
 */
+void getSongAtMoment(vi &c, vi &t, vi &moments) {
+    vi pref(c.size()+1, 0);
+    for (int i = 1; i <= c.size(); i++) {
+        pref[i] = pref[i-1] + c[i-1] * t[i-1];
+    }
+    int j = 0;
+    int i = 0;
+    while (i < moments.size()) {
+        if (moments[i] <= pref[j] || j >= pref.size()) {
+            cout << j << endl;
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
-    string line;
-    while (getline(cin, line))
+    int n, m;
+    while (cin >> n >> m)
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        vector<int> c,t;
+        while (n--) {
+            int c_, t_;
+            cin >> c_ >> t_;
+            cin.ignore();
+            c.push_back(c_);
+            t.push_back(t_);
+        }
+        string line;
+        getline(cin, line);
+        vector<int> moments;
+        read_array(line, moments);
+        getSongAtMoment(c,t,moments);
     }
 #endif
     return 0;
