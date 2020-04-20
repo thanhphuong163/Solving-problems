de8664380bde28ec92eee409ec3bdc6ff69e16d9
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-20 15:03:00 +0700

    Search_in_rotated_sorted_array_II: test solution

--- LeetCode/Search_in_rotated_sorted_array_II/solution.cpp
+++ LeetCode/Search_in_rotated_sorted_array_II/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -29,9 +29,40 @@ void print_array(vector<int> &nums) {
     puts("");
 }
 
+// Binary search
+bool bsearch(vector<int> &nums, int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+            return true;
+        else if (nums[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return false;
+}
+
 /*
-Write your solution here
+    - Find rotating point.
+    - Use binary search on the part which might contains the seeked value.
 */
+bool search(vector<int> &nums, int target) {
+    if (nums.size() == 0) return false;
+    if (nums.size() == 1) return nums[0] == target;
+    int n = nums.size();
+    int p = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (nums[i] > nums[i+1])
+            p = i+1;
+    }
+    if (nums[0] <= target && target <= nums[p-1])
+        return bsearch(nums, 0, p-1, target);
+    else if (nums[p] <= target && target <= nums[n - 1])
+        return bsearch(nums, p, n-1, target);
+    else
+        return false;
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
@@ -43,7 +74,13 @@ int main(int argc, char const *argv[]) {
     {
         vector<int> nums;
         read_array(line, nums);
-        /* your code here */
+        int target;
+        cin >> target;
+        cin.ignore();
+        if (search(nums, target))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
     }
 #endif
     return 0;
