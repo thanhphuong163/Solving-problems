c1f090febabd6a71ce97d8cdab7a23e81e90d16c
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-20 21:33:29 +0700

    Three_parts_of_the_array: test two-pointer solution

--- CodeForces/Three_parts_of_the_array/solution.cpp
+++ CodeForces/Three_parts_of_the_array/solution.cpp
@@ -46,19 +46,24 @@ int getPartition(vector<int> &nums)
     int len = nums.size();
     int left = 0;
     int right = len - 1;
-    int sum1 = 0;
-    int sum3 = 0;
+    int sum1 = nums[left];
+    int sum3 = nums[right];
     int res = 0;
     while (left < right)
     {
-        sum1 += nums[left];
-        sum3 += nums[right];
-        if (sum1 == sum3)
+        if (sum1 == sum3) {
             res = sum1;
-        else if (sum1 > sum3)
-            right--;
-        else
             left++;
+            right--;
+        }
+        else if (sum1 > sum3) {
+            right--;
+            sum3 += nums[right];
+        }
+        else {
+            left++;
+            sum1 += nums[left];
+        }
     }
     return res;
 }
