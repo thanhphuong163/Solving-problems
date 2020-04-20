d792ace44c910efee948950f600c35c5d3f2d8db
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-20 15:33:35 +0700

    Find_fist_last_in_sorted_array: test solution

--- LeetCode/Find_fist_last_in_sorted_array/solution.cpp
+++ LeetCode/Find_fist_last_in_sorted_array/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -12,16 +12,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
+void print_array(vector<int> &nums)
+{
     for (int i : nums)
     {
         cout << i << " ";
@@ -29,12 +32,50 @@ void print_array(vector<int> &nums) {
     puts("");
 }
 
+// Binary search
+int bsearch(vector<int> &nums, int target)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
 /*
-Write your solution here
+    Use binary search to find the index of target
+    Use two pointers to expand the range then.
 */
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int index = bsearch(nums, target);
+    if (index == -1)
+    {
+        return {-1, -1};
+    }
+    else
+    {
+        int left = index;
+        int right = index;
+        while (nums[index - 1] == target)
+            left--;
+        while (nums[index + 1] == target)
+            right++;
+        return {left, right};
+    }
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
