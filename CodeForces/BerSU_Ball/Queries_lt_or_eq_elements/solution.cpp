529eef9b2fa1225f32eafb98b37cc3c58669dd19
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-20 17:28:01 +0700

    Queries_lt_or_eq_elements: test solution

--- CodeForces/BerSU_Ball/Queries_lt_or_eq_elements/solution.cpp
+++ CodeForces/BerSU_Ball/Queries_lt_or_eq_elements/solution.cpp
@@ -1,28 +1,29 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://codeforces.com/problemset/problem/600/B
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<long> &nums) {
     stringstream ss(line);
-    int num;
+    long num;
     while (ss >> num) {
     	nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
-    for (int i : nums)
+void print_array(vector<long> &nums) {
+    for (long i : nums)
     {
         cout << i << " ";
     }
@@ -32,18 +33,43 @@ void print_array(vector<int> &nums) {
 /*
 Write your solution here
 */
+void query(vector<long> &a, vector<long> &b) {
+    int n = a.size();
+    int m = b.size();
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int i = 0;
+    int j = 0;
+    while (j < m) {
+        if (a[i] <= b[j]) {
+            i++;
+        }
+        else {
+            cout << i << " ";
+            j++;
+        }
+    }
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
-    string line;
-    while (getline(cin, line))
+    int m, n;
+    while (cin >> m >> n)
     {
-        vector<int> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        string line;
+        getline(cin, line);
+        vector<long> a;
+        read_array(line, a);
+
+        getline(cin, line);
+        vector<long> b;
+        read_array(line, b);
+        
+        query(a, b);
     }
 #endif
     return 0;
