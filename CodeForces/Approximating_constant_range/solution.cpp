706c96def68bb46a53cb92b2c7fd1721a312cd45
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-22 21:05:27 +0700

    Approximating_constant_range: test reading input

--- CodeForces/Approximating_constant_range/solution.cpp
+++ CodeForces/Approximating_constant_range/solution.cpp
@@ -1,29 +1,35 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://codeforces.com/problemset/problem/602/B
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #define ulli unsigned long long int
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a < b ? a : b)
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<ulli> &nums)
+{
     stringstream ss(line);
     ulli num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void prulli_array(vector<ulli> &nums) {
+void prulli_array(vector<ulli> &nums)
+{
     for (ulli i : nums)
     {
         cout << i << " ";
@@ -34,18 +40,33 @@ void prulli_array(vector<ulli> &nums) {
 /*
 Write your solution here
 */
+int getRange(vector<ulli> &nums)
+{
+    long n = nums.size();
+    long range = 0;
+    long left = 0;
+    long right = 0;
+    long min_ = nums[0];
+    long max_ = nums[0];
+
+    return 0;
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
     string line;
-    while (getline(cin, line))
+    int n;
+    while (cin >> n)
     {
+        cin.ignore();
+        getline(cin, line);
         vector<ulli> nums;
         read_array(line, nums);
-        /* your code here */
+        cout << getRange(nums) << endl;
     }
 #endif
     return 0;
