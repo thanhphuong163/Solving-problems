4b173c5b769dfc65a2899885870f31ec8453dcc8
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-22 16:19:36 +0700

    Balanced_team: test solution

--- CodeForces/Balanced_team/solution.cpp
+++ CodeForces/Balanced_team/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://codeforces.com/problemset/problem/1133/C
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -9,20 +9,25 @@
 #include <string>
 #include <vector>
 
+#define ulli unsigned long long int
+#define max(a,b) (a > b ? a : b)
+
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<ulli> &nums)
+{
     stringstream ss(line);
-    int num;
+    ulli num;
     while (ss >> num) {
     	nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<int> &nums) {
-    for (int i : nums)
+void print_array(vector<ulli> &nums)
+{
+    for (ulli i : nums)
     {
         cout << i << " ";
     }
@@ -30,8 +35,34 @@ void print_array(vector<int> &nums) {
 }
 
 /*
-Write your solution here
+    Sort array
+    Use two-pointer to determine
+    nums[end] - nums[start] <= 5
+    number of students = end - start + 1
+    1 10 17 12 15 2
+    1 2 10 12 15 17
+
 */
+long assembleTeam(vector<ulli> &nums) {
+    long size = nums.size();
+    long start = 0;
+    long end = 0;
+    long maxSize = 0;
+    sort(nums.begin(), nums.end());
+    while (start < size) {
+        if (nums[end] - nums[start] <= 5) {
+            maxSize = max(maxSize, end - start + 1);
+            if (end < size - 1)
+                end++;
+            else
+                start++;
+        }
+        else {
+            start++;
+        }
+    }
+    return maxSize;
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
@@ -39,11 +70,14 @@ int main(int argc, char const *argv[]) {
     /* Put your debugging code here */
 #else
     string line;
-    while (getline(cin, line))
+    int n;
+    while (cin >> n)
     {
-        vector<int> nums;
+        cin.ignore();
+        getline(cin, line);
+        vector<ulli> nums;
         read_array(line, nums);
-        /* your code here */
+        cout << assembleTeam(nums) << endl;
     }
 #endif
     return 0;
