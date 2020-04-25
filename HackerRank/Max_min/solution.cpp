b5a47e4ebeb488563a5d8df470d91dce926b30a5
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-25 16:55:08 +0700

    Max_min: test solution

--- HackerRank/Max_min/solution.cpp
+++ HackerRank/Max_min/solution.cpp
@@ -1,6 +1,6 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
+// Problem: https://www.hackerrank.com/challenges/angry-children/problem
 // Submit: 
 
 #include <stdio.h>
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <algorithm>
 
 #define ulli unsigned long long int
 
@@ -32,21 +34,51 @@ void print_array(vector<ulli> &nums) {
 }
 
 /*
-Write your solution here
+    sort array
+    two-pointer with k-size slide window
 */
+int maxMin(int k, vector<int> arr)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    int unfairness = INT_MAX;
+    for (int i = 0; i < n-k; i++) {
+        unfairness = min(unfairness, arr[i+k-1] - arr[i]);
+    }
+    return unfairness;
+}
 
 // #define DEBUG_MODE
+
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
-    string line;
-    while (getline(cin, line))
-    {
-        vector<ulli> nums;
-        read_array(line, nums);
-        /* your code here */
+    int n;
+    
+    while (cin >> n) {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int k;
+        cin >> k;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int arr_item;
+            cin >> arr_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            arr[i] = arr_item;
+        }
+
+        int result = maxMin(k, arr);
+
+        cout << result << "\n";
     }
+    
 #endif
     return 0;
 }
