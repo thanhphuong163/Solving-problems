3270fd23df0a25bb2b92a979ade18b319ff35029
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-27 17:47:07 +0700

    Maximal_square: test solution

--- LeetCode/Maximal_square/solution.cpp
+++ LeetCode/Maximal_square/solution.cpp
@@ -1,22 +1,27 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/maximal-square/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #define ulli unsigned long long int
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<char> &nums) {
     stringstream ss(line);
-    ulli num;
+    char num;
     while (ss >> num) {
     	nums.push_back(num);
     }
@@ -32,20 +37,70 @@ void print_array(vector<ulli> &nums) {
 }
 
 /*
-Write your solution here
+    if (matrix[i][j] == '1') {
+        minCell = min{dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}
+        dp[i][j] = sqr(sqrt(min)+1);
+    }
+    else {
+        dp[i][j] = 0;
+    }
 */
+int maximalSquare(vvc &matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+    if (n == 0 || m == 0)
+        return 0;
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    int maxSquare = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (matrix[i][0] == '1')
+        {
+            dp[i][0] = 1;
+            maxSquare = 1;
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (matrix[0][i] == '1')
+        {
+            dp[0][i] = 1;
+            maxSquare = 1;
+        }
+    }
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (matrix[i][j] == '1') {
+                int minCell = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));
+                dp[i][j] = (int)pow((sqrt(minCell) + 1), 2);
+                maxSquare = max(maxSquare, dp[i][j]);
+            }
+        }
+    }
+    return maxSquare;
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
+    int m, n;
     string line;
-    while (getline(cin, line))
+    while (cin >> m >> n)
     {
-        vector<ulli> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        vvc matrix;
+        while (m--) {
+            getline(cin, line);
+            vector<char> row;
+            read_array(line, row);
+            matrix.push_back(row);
+        }
+        cout << maximalSquare(matrix) << endl;
     }
 #endif
     return 0;
