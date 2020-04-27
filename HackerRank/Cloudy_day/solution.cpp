b979a67ef8a6fc2a6215641f5a92b88d6caea78a
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-28 00:39:33 +0700

    Cloudy_day: test solution

--- HackerRank/Cloudy_day/solution.cpp
+++ HackerRank/Cloudy_day/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://www.hackerrank.com/challenges/cloudy-day/problem
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -10,20 +10,24 @@
 #include <vector>
 
 #define ulli unsigned long long int
+#define vulli vector<ulli>
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<ulli> &nums)
+{
     stringstream ss(line);
     ulli num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<ulli> &nums) {
+void print_array(vector<ulli> &nums)
+{
     for (ulli i : nums)
     {
         cout << i << " ";
@@ -32,20 +36,71 @@ void print_array(vector<ulli> &nums) {
 }
 
 /*
-Write your solution here
+    Iterate each town:
+        - under_cloud = -1
+        - Iterate each cloud:
+            - if (y[cloud] - r[cloud] <= x[town] && x[town] <= y[cloud] + r[cloud]) then 
+                - if under_cloud == -1 then under_cloud = cloud
+                - else under_cloud = -2;
+        - If under_cloud == -1 then sunny_pop += p[town]
+        - If under_cloud != -2 then pop_cloud[under_cloud] += p[town]
+    Iterate each cloud:
+        - maxPopulation = max(pop_cloud[cloud], maxPopulation)
+    return sunny_pop + maxPopulation
+
 */
+long maximumPeople(vulli p, vulli x, vulli y, vulli r)
+{
+    long n = p.size();
+    long m = y.size();
+    ulli sunnyPeople = 0;
+    vulli cloudPeople(m, 0);
+    for (long town = 0; town < n; town++) {
+        long underCloud = -1;
+        for (int cloud = 0; cloud < m; cloud++) {
+            if (y[cloud] - r[cloud] <= x[town] && x[town] <= y[cloud] + r[cloud]) {
+                if (underCloud == -1)
+                    underCloud = cloud;
+                else
+                    underCloud = -2;    // under more than one cloud
+            }
+        }
+        if (underCloud == -1)
+            sunnyPeople += p[town];
+        if (underCloud != -2)
+            cloudPeople[underCloud] += p[town];
+    }
+    ulli maxPeople = 0;
+    for (long cloud = 0; cloud < m; cloud++) {
+        maxPeople = max(maxPeople, cloudPeople[cloud]);
+    }
+    return (long) maxPeople+sunnyPeople;
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
+    ulli n, m;
     string line;
-    while (getline(cin, line))
+    while (cin >> n)
     {
-        vector<ulli> nums;
-        read_array(line, nums);
-        /* your code here */
+        cin.ignore();
+        vulli p, x;
+        getline(cin, line);
+        read_array(line, p);
+        getline(cin, line);
+        read_array(line, x);
+        cin >> m;
+        cin.ignore();
+        vulli y, r;
+        getline(cin, line);
+        read_array(line, y);
+        getline(cin, line);
+        read_array(line, r);
+        cout << maximumPeople(p, x, y, r) << endl;
     }
 #endif
     return 0;
