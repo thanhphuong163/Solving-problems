b2bba7aa728e9d0598af26a7c65180a50bf6e01e
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-30 15:10:51 +0700

    Binary_tree_maximum_path_sum: test reading input

--- LeetCode/Binary_tree_maximum_path_sum/solution.cpp
+++ LeetCode/Binary_tree_maximum_path_sum/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -14,16 +14,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<ulli> &nums)
+{
     stringstream ss(line);
     ulli num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<ulli> &nums) {
+void print_array(vector<ulli> &nums)
+{
     for (ulli i : nums)
     {
         cout << i << " ";
@@ -34,9 +37,38 @@ void print_array(vector<ulli> &nums) {
 /*
 Write your solution here
 */
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+int helper(TreeNode *node, int &maxSum)
+{
+    if (node == NULL)
+        return 0;
+    int left = helper(node->left, maxSum);
+    int right = helper(node->right, maxSum);
+    int leftOrRightOrNode = max(node->val, node->val + max(left, right));
+    maxSum = max(maxSum, max(node->val + left + right, leftOrRightOrNode));
+    return leftOrRightOrNode;
+}
+
+int maxPathSum(TreeNode *root)
+{
+    int maxSum = INT_MIN;
+    helper(root, maxSum);
+    return maxSum;
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
@@ -45,7 +77,7 @@ int main(int argc, char const *argv[]) {
     {
         vector<ulli> nums;
         read_array(line, nums);
-        /* your code here */
+        print_array(nums);
     }
 #endif
     return 0;
