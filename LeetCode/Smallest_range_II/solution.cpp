f06f05f1ca242c7e954c48f43effecd9403b6055
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-06-09 14:43:39 +0700

    Smallest_range_II: test IO

--- LeetCode/Smallest_range_II/solution.cpp
+++ LeetCode/Smallest_range_II/solution.cpp
@@ -1,7 +1,7 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
-// Submit: 
+// Problem: https://leetcode.com/problems/smallest-range-ii/
+// Submit:
 
 #include <stdio.h>
 #include <iostream>
@@ -14,16 +14,19 @@
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<ulli> &nums)
+{
     stringstream ss(line);
     ulli num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<ulli> &nums) {
+void print_array(vector<ulli> &nums)
+{
     for (ulli i : nums)
     {
         cout << i << " ";
@@ -34,9 +37,23 @@ void print_array(vector<ulli> &nums) {
 /*
 Write your solution here
 */
+ulli smallestRangeII(vector<ulli> &A, ulli K)
+{
+    int size = A.size();
+    ulli min = A[0], max = A[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (A[i] < min)
+            min = A[i];
+        if (A[i] > max)
+            max = A[i];
+    }
+    return max - min;
+}
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
@@ -45,7 +62,9 @@ int main(int argc, char const *argv[]) {
     {
         vector<ulli> nums;
         read_array(line, nums);
-        /* your code here */
+        ulli k;
+        cin >> k;
+        cout << smallestRangeII(nums, k) << endl;
     }
 #endif
     return 0;
