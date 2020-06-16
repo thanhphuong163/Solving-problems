b7cc8fb1470cf1880b207c25cd40ceb1e54b7501
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-06-16 15:25:15 +0700

    Spreadsheet: test print output

--- CodeForces/Spreadsheet/solution.cpp
+++ CodeForces/Spreadsheet/solution.cpp
@@ -62,7 +62,7 @@ void executeTask(vvulli ss, vvi tasks) {
         int maxx = 0;
         for(int j = 0; j < m; j++) {
             if (memCol[j] < i) {
-                for (int k = i; k < n; k++)
+                for (int k = i; k < n-1; k++)
                 {
                     if (ss[k + 1][j] < ss[k][j])
                     {
@@ -75,6 +75,7 @@ void executeTask(vvulli ss, vvi tasks) {
         }
         memRow[i] = maxx;
     }
+    memRow[n-1] = n-1;
     // for (int t = 0; t < k; t++) {
     //     int l = tasks[t][0] - 1;
     //     int r = tasks[t][1] - 1;
