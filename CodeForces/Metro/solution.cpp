d0105a161817e726b922ca8a58cff12255aa08cd
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-08-21 19:43:34 +0700

    Metro: test solution

--- CodeForces/Metro/solution.cpp
+++ CodeForces/Metro/solution.cpp
@@ -71,9 +71,16 @@ void checkRoute(vi &forward, vi &backward, int alice) {
             prev = i;
         }
     }
-    if (checkAlice(metro, 0, alice)) cout << "YES";
-    else cout << "NO";
-    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "i: ";
+        for (int j = 0; j < metro[i].size(); j++) {
+            cout << metro[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // if (checkAlice(metro, 0, alice)) cout << "YES";
+    // else cout << "NO";
+    // cout << endl;
 }
 
 // #define DEBUG_MODE
