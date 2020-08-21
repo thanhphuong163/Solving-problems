851ddae79726d15c50f05fd2397834ce18db42a9
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-08-21 16:05:32 +0700

    Love_triangle: Test solution

--- CodeForces/Love_triangle/solution.cpp
+++ CodeForces/Love_triangle/solution.cpp
@@ -1,6 +1,6 @@
 // Author: Nguyen Thanh Phuong
 // Email: thanhphuong.its@gmail.com
-// Problem: 
+// Problem: https://codeforces.com/problemset/problem/939/A
 
 #include <stdio.h>
 #include <iostream>
@@ -12,13 +12,15 @@
 
 #define ulli unsigned long long int
 #define vulli vector<ulli>
+#define vi vector<int>
+#define vvi vector<vi>
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<ulli> &nums) {
+void read_array(string line, vector<int> &nums) {
     stringstream ss(line);
-    ulli num;
+    int num;
     while (ss >> num) {
     	nums.push_back(num);
     }
@@ -34,20 +36,49 @@ void print_array(vector<ulli> &nums) {
 }
 
 /*
-Write your solution here
+check g[g[g[i]]] == i
 */
+void addEdge(vvi &graph, int v, int u) {
+    graph[v].push_back(u);
+}
+
+int getCrush(vvi &graph, int i) {
+    return graph[i][0];
+}
+
+void loveTriangle(vi arr) {
+    int n = arr.size();
+    vvi g(n, vi());
+    for (int i = 0; i < n; i++) {
+        addEdge(g, i, arr[i]-1);
+    }
+    bool flag = false;
+    for (int i = 0; i < n; i++) {
+        int crush = getCrush(g, getCrush(g, getCrush(g,i)));
+        if (crush == i) {
+            flag = true;
+            break;
+        }
+    }
+    if (flag) cout << "YES";
+    else cout << "NO";
+    cout << endl;
+}
 
 // #define DEBUG_MODE
 int main(int argc, char const *argv[]) {
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
+    int n;
     string line;
-    while (getline(cin, line))
+    while (cin >> n)
     {
-        vector<ulli> nums;
+        cin.ignore();
+        getline(cin, line);
+        vector<int>nums;
         read_array(line, nums);
-        /* your code here */
+        loveTriangle(nums);
     }
 #endif
     return 0;
