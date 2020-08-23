c023a1cd958dc7f5313723a2b6e4d75e81e8bf1f
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-08-23 13:29:02 +0700

    Balls_game: test two-pointer solution with test #4

--- CodeForces/Balls_game/solution.cpp
+++ CodeForces/Balls_game/solution.cpp
@@ -13,21 +13,24 @@
 #define ulli unsigned long long int
 #define vulli vector<ulli>
 #define vi vector<int>
-#define max(a,b) a > b ? a : b
+#define max(a, b) a > b ? a : b
 
 using namespace std;
 
 // Read input
-void read_array(string line, vector<int> &nums) {
+void read_array(string line, vector<int> &nums)
+{
     stringstream ss(line);
     int num;
-    while (ss >> num) {
-    	nums.push_back(num);
+    while (ss >> num)
+    {
+        nums.push_back(num);
     }
 }
 
 // Print out input
-void print_array(vector<ulli> &nums) {
+void print_array(vector<ulli> &nums)
+{
     for (ulli i : nums)
     {
         cout << i << " ";
@@ -36,28 +39,39 @@ void print_array(vector<ulli> &nums) {
 }
 
 /*
-Write your solution here
+    Use two-pointer technique
+    Iterate over the balls
+    when balls[i] == x then use left and right pointer to expand
+    max length = right - left + 1
 */
-int play(vi &balls, int x) {
+int play(vi &balls, int x)
+{
     int n = balls.size();
     int max_len = 0;
     int i = 0;
-    while (i < n) {
-        if (balls[i] == x) {
+    while (i < n)
+    {
+        if (balls[i] == x)
+        {
             int l = i;
             int r = i;
-            while (balls[r+1] == x) r++;
+            while (balls[r + 1] == x)
+                r++;
             i = r;
-            if (r-l+1 >= 2) {
-                while(balls[l-1] == balls[r+1]) {
+            int tmp = r - l + 1;
+            if (tmp >= 2)
+            {
+                while (balls[l - 1] == balls[r + 1])
+                {
                     l--;
                     r++;
                     while (balls[r + 1] == balls[r])
                         r++;
                     while (balls[l - 1] == balls[l])
                         l--;
+                    tmp = (r - l + 1) - tmp;
+                    if (tmp <= 2) break;
                 }
-                max_len = max(max_len, r-l+1);
             }
         }
         i++;
@@ -66,7 +80,8 @@ int play(vi &balls, int x) {
 }
 
 // #define DEBUG_MODE
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 #ifdef DEBUG_MODE
     /* Put your debugging code here */
 #else
