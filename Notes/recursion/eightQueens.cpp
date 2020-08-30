f2cc40d8a0f438a5220a3637e006f14682ef3f88
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-08-30 12:04:56 +0700

    solved eight-queen problem

---
+++ Notes/recursion/eightQueens.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printArray(vector<int> &arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+void putOnBoard(int row, vector<int>& board) {
+    int n = board.size();
+    if (row == n) {
+        printArray(board);
+    }
+    else {
+        vector<int> feasible(n, 1);
+        for (int i = 0; i < row; ++i) {
+            feasible[board[i]] = 0;
+            int a = board[i] - (row-i);
+            int b = board[i] + (row-i);
+            if (a >= 0) feasible[a] = 0;
+            if (b < n) feasible[b] = 0;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (feasible[i]) {
+                board[row] = i;
+                putOnBoard(row+1, board);
+            }
+        }
+    }
+}
+
+void solveEightQueens(int n) {
+    vector<int> board(n, -1);
+    putOnBoard(0, board);
+}
+
+int main(int argc, char const *argv[])
+{
+    int n = 4;
+    cout << "Enter an integer: ";
+    cin >> n;
+    solveEightQueens(n);
+    return 0;
+}
