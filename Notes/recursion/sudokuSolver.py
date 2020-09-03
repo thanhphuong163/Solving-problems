3e807a1ea49b73a81684aa6dd58946949b0dcfaa
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-09-03 23:03:57 +0700

    added sudoku solver

---
+++ Notes/recursion/sudokuSolver.py
@@ -0,0 +1,85 @@
+import numpy as np
+import time
+
+
+def possible(board, row, col, n):
+    if n in board[row, :]:
+        return False
+    if n in board[:, col]:
+        return False
+    blRow = row//3
+    blCol = col//3
+    if n in board[blRow*3: (blRow+1)*3, blCol*3: (blCol+1)*3]:
+        return False
+    return True
+
+
+def solveBacktracking(board):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if board[i, j] == 0:
+                for n in range(1, 10):
+                    if possible(board, i, j, n):
+                        board[i, j] = n
+                        solveBacktracking(board)
+                        board[i, j] = 0
+                return
+    # print(board)
+
+
+def chooseCell(board):
+    col = -1
+    row = -1
+    minOpt = 1000
+    for i in range(0, 9):
+        for j in range(0, 9):
+            count = 0
+            if board[i, j] == 0:
+                for n in range(1, 10):
+                    if possible(board, i, j, n):
+                        count += 1
+                if count < minOpt:
+                    row = i
+                    col = j
+                    minOpt = count
+    return (row, col)
+
+
+def solveSmartBacktracking(board):
+    i, j = chooseCell(board)
+    if (i == -1):
+        return
+    for n in range(1, 10):
+        if possible(board, i, j, n):
+            board[i, j] = n
+            solveSmartBacktracking(board)
+            board[i, j] = 0
+
+
+if __name__ == "__main__":
+    board = np.array([[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                      [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                      [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                      [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                      [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                      [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                      [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                      [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                      [0, 0, 0, 0, 8, 0, 0, 7, 9]])
+    startTime = time.time_ns()
+    solveBacktracking(board)
+    runningTime = time.time_ns() - startTime
+    print("Backtracking: ", runningTime/1e9)
+    board1 = np.array([[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                       [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                       [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                       [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                       [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                       [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                       [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                       [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                       [0, 0, 0, 0, 8, 0, 0, 7, 9]])
+    startTime = time.time_ns()
+    solveSmartBacktracking(board1)
+    runningTime = time.time_ns() - startTime
+    print("Smart Backtracking: ", runningTime/1e9)
