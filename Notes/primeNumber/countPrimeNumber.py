a13415177f889c3a7b7057ef5990c751ae192d76
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-09-19 22:10:02 +0700

    added countPrimeNumber

--- Notes/primeNumber/countPrimeNumber.py
+++ Notes/primeNumber/countPrimeNumber.py
@@ -1,25 +1,28 @@
 import numpy as np
+import time
 
-# O(n log(log(n)))
+
 def countPrime(n):
     isPrime = np.ones((n+1), dtype=int)
     isPrime[0] = 0
     isPrime[1] = 0
     i = 2
+    start_time = time.time()
     while i*i <= n:
         if isPrime[i] == 1:
             for j in range(i*i, n+1, i):
                 isPrime[j] = 0
         i += 1
+    print(time.time() - start_time)
     return isPrime.sum()
 
-# O(n)
 def countPrimeV2(n):
     isPrime = np.ones((n+1), dtype=int)
     prime = []
     spf = [None] * (n+1)
     isPrime[0] = 0
     isPrime[1] = 0
+    start_time = time.time()
     for i in range(2, n):
         if isPrime[i] == 1:
             prime.append(i)
@@ -29,7 +32,8 @@ def countPrimeV2(n):
             isPrime[i * prime[j]] = 0
             spf[i * prime[j]] = prime[j]
             j += 1
+    print(time.time() - start_time)
     return len(prime)
 
-n = 30
+n = 10000000
 print(countPrime(n) == countPrimeV2(n))
