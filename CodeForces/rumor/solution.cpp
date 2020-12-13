afb1752be005ee4a2b2938027ecd3af1f02b1c28
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-12-13 21:57:33 +0700

    rumor: test solution

--- CodeForces/rumor/solution.cpp
+++ CodeForces/rumor/solution.cpp
@@ -58,20 +58,60 @@ void printArray(vi &nums) {
 }
 
 /*
-Write your solution here
+ create a graph of friends
+ find the character who has the lowest cost to bribe in each connected component
+ use a heard_rumor[] to store people who heard the rumor
+ total cost will be the sum of lowest cost character in each connected component
 */
 
+
+int explore(vvi &graph, vi &c, int v, vector<bool> &visited) {
+    visited[v] = true;
+    int cost = c[v];
+    for (int i = 0; i < graph[v].size(); i++) {
+        if (!visited[graph[v][i]]) {
+            int tmp = explore(graph, c, graph[v][i], visited);
+            cost = cost < tmp ? cost : tmp;
+        }        
+    }
+    return cost;
+}
+
+int spread_rumor(vi &c, vi &x, vi &y) {
+    int n = c.size();
+    vvi graph = vvi(n, vi);
+    int m = x.size();
+    for (int i = 0; i < m; i++) {
+        graph[x[i]-1].push_back(y[i]-1);
+        graph[y[i]-1].push_back(x[i]-1);
+    }
+    int cost = 0;
+    vector<bool> heard_rumor = vector<bool>(n, false);
+    for (int i = 0; i < n; i++) {
+        if (!heard_rumor[graph[i]]) {
+            cost += explore(graph, c, i, heard_rumor);
+    }
+    return cost;
+}
 int main(int argc, char const *argv[]) {
 #if DEBUG_MODE == 1
     /* Put your debugging code here */
 #else
+    int n, m;
+    cin >> n >> m;
+    cin.ignore();
     string line;
-    while (getline(cin, line))
-    {
-        vi nums;
-        readArray(line, nums);
-        /* your code here */
+    vi c;
+    getline(cin, line);
+    readArray(line, c);
+    vi x, y;
+    for (int i = 0; i < m; i++) {
+        int xi, yi;
+        cin >> xi >> yi;
+        x.push_back(xi);
+        y.push_back(yi);
     }
+    cout << spread_rumor(c, x, y) << endl;
 #endif
     return 0;
 }
