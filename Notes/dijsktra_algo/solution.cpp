e8b9fa9edd82cf61fc48727e15e9120815950bb0
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2021-04-28 21:34:44 -0600

    dijsktra_algo: test

--- Notes/dijsktra_algo/solution.cpp
+++ Notes/dijsktra_algo/solution.cpp
@@ -90,7 +90,7 @@ int main(int argc, char const *argv[]) {
         cin.ignore();
         add_edge(graph, u-1, v-1, w);
     }
-    cout << graph.size() << endl;
+    cout << graph[0][0].first << endl;
     find_shortest_path(graph, s-1);
 #endif
     return 0;
